9494469c0b6da5d914546caa051194e794315929
sylbibae <107978646+sylbibae@users.noreply.github.com>
2022-07-19 19:28:01 -0400

    Update spindler_battery.py

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,9 +1,12 @@
-from battery import Battery
+from battery.battery import Battery
+from utils import add_years_to_date
+
 
 class SpindlerBattery(Battery):
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
+    def __init__(self, current_date, last_service_date):
         self.current_date = current_date
+        self.last_service_date = last_service_date
 
     def needs_service(self):
-        return self.current_date - self.last_service_date > 2
+        date_to_service = add_years_to_date(self.last_service_date, 3)
+        return date_to_service < current_date
