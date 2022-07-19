a6a4d8bc11e37b28f16bf655be170baeb4de1c58
sylbibae <107978646+sylbibae@users.noreply.github.com>
2022-07-19 13:03:45 -0400

    Add files via upload

---
+++ test/car_factory_test.py
@@ -0,0 +1,24 @@
+import unittest
+from datetime import datetime
+
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+
+class TestCarFactory(unittest.TestCase):
+    def test_calliope(self):
+        current_date = datetime.today.date()
+        last_service_date = current_date.replace(year=current_date.year - 3)
+        current_mileage = 0
+        last_service_mileage = 0
+
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        self.assertEqual(car, car_factory.create_calliope(current_date, last_service_date, current_mileage, last_service_mileage))
+
+
