7d4f074b7724b78ab638315e63077e581e1cfe47
sylbibae <107978646+sylbibae@users.noreply.github.com>
2022-07-19 14:22:25 -0400

    Add files via upload

---
+++ tires/octoprime_tires.py
@@ -0,0 +1,14 @@
+from tires import Tires 
+
+class OctoprimeTires(Tires):
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+
+    def needs_service(self, tire_wear):
+        sum = 0
+        for tire in tire_wear:
+            sum += tire
+        if sum >= 3:
+            return True
+        else:
+            return False
