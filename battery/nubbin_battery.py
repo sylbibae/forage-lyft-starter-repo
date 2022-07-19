539208008d7b00ccd90346f7383771b9bd3fd09b
sylbibae <107978646+sylbibae@users.noreply.github.com>
2022-07-19 19:22:19 -0400

    Update nubbin_battery.py

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -3,9 +3,9 @@ from utils import add_years_to_date
 
 
 class NubbinBattery(Battery):
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
+    def __init__(self, current_date, last_service_date):
         self.current_date = current_date
+        self.last_service_date = last_service_date
 
     def needs_service(self):
         date_to_service = add_years_to_date(self.last_service_date, 4)
